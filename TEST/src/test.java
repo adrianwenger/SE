2d57e85bcabfac2b617114d17520facf91d1b176
Adrian Wenger <adwenger@htwg-konstanz.de>
2014-10-16 14:55:38 +0200

    First Eclipse Class

---
+++ TEST/src/test.java
@@ -0,0 +1,14 @@
+
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		int a = 12345;
+		System.out.println(a);
+	}
+
+}
