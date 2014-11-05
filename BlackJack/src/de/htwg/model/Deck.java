dd344913b61235c4d1b11079eddcd4039207730e
philippschultheiss <philippschultheiss@172.20.129.58>
2014-11-05 12:58:09 +0100

    Modified de.htwg.model-----> Siut.java, Card.java, Deck.java

--- BlackJack/src/de/htwg/model/Deck.java
+++ BlackJack/src/de/htwg/model/Deck.java
@@ -9,41 +9,59 @@ import java.util.Random;
 public class Deck {
 
     private Card[] myCards;
+
+    public Card[] getMyCards() {
+        return myCards;
+    }
+
+    public int getNumCards() {
+        return numCards;
+    }
+
+    public int getNumDeck() {
+        return numDeck;
+    }
     //Number of cards currently in the deck
     private int numCards;
+    private int numDeck;
+    //Card index
+    private int index = 0;
+    public static Random rmd = new Random();
 
     //Constructor which defines the number of decks and shuffle 
     public Deck(int numDeck, boolean shuffle) {
-        //
+        //Initialise size of myCards
+        this.numDeck = numDeck;
         this.numCards = numDeck * 52;
-        this.myCards = new Card[this.numCards];
-        //Card index
-        int index = 0;
+        this.myCards = new Card[this.numCards]; 
+     }
+    
 
+    public void createDeck() {
         //create Deck
         //for each deck
         for (int i = 0; i < numDeck; i++) {
             //for each suit
             for (int j = 0; j < 4; j++) {
                 //for each number
-                for (int z = 0; z <= 13; z++) {
+                for (int z = 1; z <= 13; z++) {
                     //add new card to deck
-                    this.myCards[index] = new Card(Suit.values()[s], n);
-
+                    this.myCards[index] = new Card(Suit.values()[j], z);
+                    index++;
                 }
             }
         }
-        //    //this method shuffles the cards and returns a mixed deck
-//    public static int[] shuffleCards(int [] card) {
-//        int tmp;
-//        int rand;
-//        for (int i = 0; i < card.length; i++) {
-//            rand = rmd.nextInt(card.length);
-//            tmp = card[i];
-//            card[i] = card[rand];
-//            card[rand] = tmp;
-//        }
-//        return card;
-//    }
+    }
+    
+    //this method shuffles the cards and returns a mixed deck
+    public static void shuffleCards(Card[] myCards) {
+        Card tmp;
+        int rand;
+        for (int i = 0; i < myCards.length; i++) {
+            rand = rmd.nextInt(myCards.length);
+            tmp = myCards[i];
+            myCards[i] = myCards[rand];
+            myCards[rand] = tmp;
+        }
     }
 }
--- BlackJack/src/de/htwg/view/tui.java
+++ BlackJack/src/de/htwg/view/tui.java
@@ -1,12 +1,15 @@
 package de.htwg.view;
 
 import de.htwg.model.Card;
+import de.htwg.model.Deck;
+import de.htwg.model.Suit;
 
 public class tui {
 
 public static void main(String[] args) {
-      Card test = new Card();
-      System.out.println(test);      
+      Deck test = new Deck(2, true);
+      System.out.println(test.getMyCards());
+      
     }
 
 }
