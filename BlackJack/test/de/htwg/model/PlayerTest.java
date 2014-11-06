d35c61f38bc3ffdcdfec445e1224ab218e62902c
Adrian Wenger <adwenger@htwg-konstanz.de>
2014-11-06 15:28:16 +0100

    Testing Environment for Class Card improved

--- BlackJack/test/de/htwg/model/PlayerTest.java
+++ BlackJack/test/de/htwg/model/PlayerTest.java
@@ -34,8 +34,6 @@ public class PlayerTest {
 
     @Test
     public void testSomeMethod() {
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
     
 }
