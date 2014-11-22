067a908243dd6fea15b59faf4b081a0e4ab0ea13
Adrian Wenger <adwenger@htwg-konstanz.de>
2014-11-22 13:33:50 +0100

    improvements regarding checkstyle and Code coverage

--- SE2014WS-16-BlackJack/src/main/java/de/htwg/model/Player.java
+++ SE2014WS-16-BlackJack/src/main/java/de/htwg/model/Player.java
@@ -1,89 +1,135 @@
 package de.htwg.model;
 
-import java.util.ArrayList;
-import java.util.List;
+/**
+ *
+ * @author Adi
+ */
+public final class Player {
 
-public class Player {
-    
+    /**
+     *
+     */
     private static final int ZERO = 0;
+    /**
+     *
+     */
     private static final int ELEVEN = 11;
+    /**
+     *
+     */
     private static final int TEN = 10;
+    /**
+     *
+     */
     private static final int ONE = 1;
-    private final int MAXCARDS = 10;
-    private final int BLACKJACK = 21;
-    //Value of players hand
+    /**
+     *
+     */
+    private static final int MAXCARDS = 10;
+    /**
+     *
+     */
+    private static final int BLACKJACK = 21;
+    /**
+     * Value of players hand.
+     */
     private int playerVal;
-    //Value of a card
+    /**
+     * Value of a card.
+     */
     private int numOfCards;
-    //name from player
+    /**
+     * name from player.
+     */
     private final String name;
-    //players hand
-    private Card[] playerHand = new Card[MAXCARDS];
+    /**
+     * players hand.
+     */
+    private final Card[] playerHand = new Card[MAXCARDS];
 
-    public Player(String name) {
-        this.name = name;
+    /**
+     *
+     * @param playerName Player Name
+     */
+    public Player(final String playerName) {
+        this.name = playerName;
         this.clearHand();
     }
 
+    /**
+     *
+     * @return name
+     */
     public String getName() {
         return name;
     }
 
-    public int getPlayerVal() {
-        return playerVal;
+    /**
+     * Reset players hand.
+     */
+    private void clearHand() {
+        for (int i = ZERO; i < MAXCARDS; i++) {
+            this.playerHand[i] = null;
+        }
     }
 
-    //Reset players hand
-    private void clearHand() {
-        for(int i = ZERO; i < MAXCARDS; i++){
-            this.playerHand[i] = null;
-        }  
-    }
-    
-    //add card to users hand
-    public boolean add(Card card){
-        if(this.numOfCards == MAXCARDS){
-            System.err.printf("%s's hand already has 10 cards!\n", this.name);
-            System.exit(ZERO);
+    /**
+     * add card to users hand.
+     *
+     * @return added correctly?
+     * @param card Card to be added
+     */
+    public boolean add(final Card card) {
+        if (this.numOfCards == MAXCARDS) {
+            return false;
         }
+        // add Card to Player
         this.playerHand[this.numOfCards] = card;
         this.numOfCards++;
-        
-        return(this.getValue() <= BLACKJACK);
+
+        return (this.getValue() <= BLACKJACK);
     }
-    
-    //Returns the value of players hand
-    private int getValue(){
+
+    /**
+     * Returns the value of players hand.
+     *
+     * @return Card Value
+     */
+    public int getValue() {
         this.playerVal = ZERO;
         int cardNum;
         int numAces = ZERO;
-        for(int i = ZERO; i < this.numOfCards; i++){
+        for (int i = ZERO; i < this.numOfCards; i++) {
             cardNum = this.playerHand[i].getNumber();
             //ACES
-            if(cardNum == ONE){
+            if (cardNum == ONE) {
                 numAces++;
                 playerVal += ELEVEN;
-            //FACE CARD    
-            }else if(cardNum > TEN){
+                //FACE CARD
+            } else if (cardNum > TEN) {
                 playerVal += TEN;
-            //NUMBERS
-            }else{
+                //NUMBERS
+            } else {
                 playerVal += cardNum;
             }
         }
         //ABFRAGE Ob >21 dann ACES = 1 sonst ACES = 11
-        while(playerVal > BLACKJACK && numAces > ZERO){
+        while (playerVal > BLACKJACK && numAces > ZERO) {
             playerVal -= TEN;
             numAces--;
         }
         return playerVal;
     }
-    //Returns the hand from the player
-    public void printPlayersHand(){
+
+    /**
+     * Returns the hand from the player.
+     *
+     */
+    public void printPlayersHand() {
         StringBuilder sb = new StringBuilder();
         sb.append("Cards: ");
         for (Card card : playerHand) {
-            if(card == null){
+            if (card == null) {
                 break;
             }
             sb.append(card).append(" ");
@@ -91,9 +137,12 @@ public class Player {
         System.out.println(sb.toString());
         System.out.printf("Value: %d \n", playerVal);
     }
-    
-    //Checks BlackJack
-    public boolean blackJack(){
+
+    /**
+     * Checks BlackJack.
+     * @return blackjack?
+    */
+    public boolean blackJack() {
         return playerVal == BLACKJACK;
     }
 }
