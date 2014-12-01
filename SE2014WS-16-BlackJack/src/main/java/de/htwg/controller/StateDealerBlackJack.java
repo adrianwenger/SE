0a5ae2c02aeacd820de92dc61ccb76bfb021ff6d
Adrian Wenger <adwenger@htwg-konstanz.de>
2014-12-01 14:49:28 +0100

    State Pattern implemented

---
+++ SE2014WS-16-BlackJack/src/main/java/de/htwg/controller/StateDealerBlackJack.java
@@ -0,0 +1,13 @@
+package de.htwg.controller;
+
+/**
+ *
+ * @author Adrian Wenger
+ */
+public class StateDealerBlackJack {
+
+    public void change(BlackJackController controller) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
