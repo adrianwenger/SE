6ccbb84f762b8a2f996e5c32681957ebcd1e6e3f
phschult <phschult@htwg-konstanz.de>
2014-12-16 09:18:05 +0100

    Create JOptionMessageDialog to create a new Game

--- SE2014WS-16-BlackJack/src/main/java/de/htwg/blackjack/aview/gui/BlackJackFrame.java
+++ SE2014WS-16-BlackJack/src/main/java/de/htwg/blackjack/aview/gui/BlackJackFrame.java
@@ -14,11 +14,14 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 /**
  *
@@ -28,15 +31,15 @@ public class BlackJackFrame extends JFrame implements IObserver {
 
     private final IBlackJackController controller;
     private Container pane;
-    private static final int WIDTH = 200;
-    private static final int HEIGHT = 200;
+    private static final int WIDTH = 600;
+    private static final int HEIGHT = 600;
 
     public BlackJackFrame(final IBlackJackController controller) {
         this.controller = controller;
         controller.addObserver(this);
 
         JMenuBar menuBar;
-        
+
         JMenuItem newGameItem, closeGameItem;
 
         JMenu fileMenu;
@@ -44,18 +47,18 @@ public class BlackJackFrame extends JFrame implements IObserver {
         /**
          * Title
          */
-        setTitle("HTWG Sudoku");
+        setTitle("HTWG BlackJack");
         /**
          * Exit Options
          */
         setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         this.addWindowListener(new WindowAdapter() {
             @Override
-            public void windowClosing(WindowEvent e){
+            public void windowClosing(WindowEvent e) {
                 JFrame frame = (JFrame) e.getSource();
                 int x = JOptionPane.showConfirmDialog(frame, "Do you really want to quit HTWG BlackJack?", "Exit", JOptionPane.YES_NO_OPTION);
-                
-                if(x == JOptionPane.YES_OPTION){
+
+                if (x == JOptionPane.YES_OPTION) {
                     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 }
             }
@@ -67,11 +70,13 @@ public class BlackJackFrame extends JFrame implements IObserver {
         setSize(WIDTH, HEIGHT);
         pane = getContentPane();
         pane.setLayout(new BorderLayout());
-
+        /**
+         * Create new Menu
+         */
         menuBar = new JMenuBar();
 
         /*
-         * File Menu
+         * BlackJack Menu
          */
         fileMenu = new JMenu("BackJack");
         fileMenu.setMnemonic(KeyEvent.VK_F);
@@ -81,11 +86,11 @@ public class BlackJackFrame extends JFrame implements IObserver {
 
         closeGameItem = new JMenuItem("Exit");
         closeGameItem.addActionListener(new CloseListener());
-        
+
         fileMenu.add(newGameItem);
         fileMenu.add(closeGameItem);
         menuBar.add(fileMenu);
-        
+
         this.setJMenuBar(menuBar);
         setVisible(true);
     }
@@ -93,12 +98,25 @@ public class BlackJackFrame extends JFrame implements IObserver {
     public void update() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+    /**
+     * Checks if input is a Integer
+     * @param argument
+     * @return 
+     */
+    private boolean isInt(String argument) {
+        try {
+            Integer.parseInt(argument);
+            return true;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+    }
 
     private class CloseListener implements ActionListener {
 
         public void actionPerformed(ActionEvent e) {
             int x = JOptionPane.showConfirmDialog(null, "Do you really want to quit HTWG BlackJack?", "Exit", JOptionPane.YES_NO_OPTION);
-            if(x == JOptionPane.YES_OPTION){
+            if (x == JOptionPane.YES_OPTION) {
                 System.exit(0);
             }
         }
@@ -107,7 +125,27 @@ public class BlackJackFrame extends JFrame implements IObserver {
     private class NewListener implements ActionListener {
 
         public void actionPerformed(ActionEvent e) {
+            //New Version------------------------------------------------------------------------------
+            JPanel panel = new JPanel();
+            // Erstellung Array vom Datentyp Object, Hinzufügen der Komponenten		
+            JTextField name = new JTextField();
             
+            // Array für unsere JComboBox
+            String comboBoxListe[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+
+            //JComboBox mit number of decks wird erstellt
+            JComboBox numOfDecks = new JComboBox(comboBoxListe);
+
+            //JComboBox wird Panel hinzugefügt
+            panel.add(numOfDecks);
+            
+            Object[] message = {"Name:", name,
+                "Number of Decks:", numOfDecks};
+
+            JOptionPane pane = new JOptionPane(message,
+                    JOptionPane.PLAIN_MESSAGE,
+                    JOptionPane.OK_CANCEL_OPTION);
+            pane.createDialog(null, "Welcome to BlackJack...").setVisible(true);
         }
 
     }
