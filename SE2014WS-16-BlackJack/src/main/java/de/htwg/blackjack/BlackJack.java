f0d90705a9fb6dc94e91d229de4dd0a8881ba731
phschult <phschult@htwg-konstanz.de>
2014-12-28 13:47:51 +0100

    Gui Klasse für RoundStake Menue erstellt, jedoch noch nicht ins Frame integriert. Beim BlackJackframe abfrage für den gesamten Spieleinsatz eingebaut

--- SE2014WS-16-BlackJack/src/main/java/de/htwg/blackjack/BlackJack.java
+++ SE2014WS-16-BlackJack/src/main/java/de/htwg/blackjack/BlackJack.java
@@ -60,7 +60,7 @@ public final class BlackJack {
      */
     public static void main(final String[] args) {
         //Create new GUI
-        //BlackJackFrame gui = new BlackJackFrame(controller);
+        BlackJackFrame gui = new BlackJackFrame(controller);
         //Create new BlackJack Object
         BlackJack game = BlackJack.getInstance();
         //Starts the TextUserInterface
--- SE2014WS-16-BlackJack/src/test/java/de/htwg/blackjack/controller/impl/CalcProfitControllerTest.java
+++ SE2014WS-16-BlackJack/src/test/java/de/htwg/blackjack/controller/impl/CalcProfitControllerTest.java
@@ -50,7 +50,6 @@ public class CalcProfitControllerTest {
     
     @Test
     public void testCalcProfit() {
-//asdasd
     }
 
     @Test
